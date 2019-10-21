719166f0be287f5a06f0304949ae03192570ccde
Josh Larson <jplhomer@gmail.com>
2019-10-20 22:05:03 -0500

    Add a simple button

---
+++ packages/Button/Button.stories.js
@@ -0,0 +1,10 @@
+import Button from "./Button";
+
+export default {
+  title: "Button"
+};
+
+export const initial = () => ({
+  components: { Button },
+  template: '<Button text="Hi there"></Button>'
+});
